46a3874c44c0de02b383b625a888b95b4540f0f3
Ricardo Serin <serin.ricardo@gmail.com>
2020-10-16 15:06:09 -0500

    Layer construction

---
+++ Capa_Entidad/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Capa_Entidad")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Capa_Entidad")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("c4e0c74b-e1a8-4312-9c6c-d0b5a55e065f")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
